c6cac03c20a1afbd9ae9398a37814de56537ba5c
hasi96 <hasitha.chikkala@gmail.com>
2018-03-23 12:27:07 -0700

    plot_pca

--- plot_pca.m
+++ plot_pca.m
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 num_components=3;
 figure('units','normalized','outerposition',[0 0 1 1]);
 SensorNames = {'ALX','ALY','ALZ','ARX','ARY','ARZ','EMG0L','EMG0R','EMG1L','EMG1R','EMG2L','EMG2R','EMG3L','EMG3R','EMG4L','EMG4R','EMG5L','EMG5R','EMG6L','EMG6R','EMG7L','EMG7R','GLX','GLY','GLZ','GRX','GRY','GRZ','OPL','OPR','ORL','ORR','OYL','OYR'};
 %SensorNames = {'GLX','GLY','GLZ','GRX','GRY','GRZ'};
 
 colorspec = {[0.9 0.3 0.9];[0.6 0.5 0.2];[0.9 0.8 0.1];[0.8 1 0];[0 1 1];[1 0 0];[0 1 0];[0 0 1]; [1 0.5 0.1];[0 0 0];};
+=======
+SensorNames = {'ALX','ALY','ALZ','ARX','ARY','ARZ','EMG0L','EMG0R','EMG1L','EMG1R','EMG2L','EMG2R','EMG3L','EMG3R','EMG4L','EMG4R','EMG5L','EMG5R','EMG6L','EMG6R','EMG7L','EMG7R','GLX','GLY','GLZ','GRX','GRY','GRZ','OPL','OPR','ORL','ORR','OYL','OYR'}
+
+>>>>>>> 2ba6cd6ef62e29c151a0637fc621758b5c9235f7
 for i = 1:num_components
     x = 1:34;
     y = [];
