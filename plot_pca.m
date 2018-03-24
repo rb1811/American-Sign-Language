da07dc7552e5825b1f1c0d664a4b1c4a3e59d7ab
srinijakanteti <skantet1@asu.edu>
2018-03-23 21:33:45 -0700

    linux working code

--- plot_pca.m
+++ plot_pca.m
@@ -1,23 +1,39 @@
-SensorNames = {'ALX','ALY','ALZ','ARX','ARY','ARZ','EMG0L','EMG0R','EMG1L','EMG1R','EMG2L','EMG2R','EMG3L','EMG3R','EMG4L','EMG4R','EMG5L','EMG5R','EMG6L','EMG6R','EMG7L','EMG7R','GLX','GLY','GLZ','GRX','GRY','GRZ','OPL','OPR','ORL','ORR','OYL','OYR'}
 
+num_components=3;
+% figure('units','normalized','outerposition',[0 0 1 1]);
+SensorNames = {'ALX','ALY','ALZ','ARX','ARY','ARZ','EMG0L','EMG0R','EMG1L','EMG1R','EMG2L','EMG2R','EMG3L','EMG3R','EMG4L','EMG4R','EMG5L','EMG5R','EMG6L','EMG6R','EMG7L','EMG7R','GLX','GLY','GLZ','GRX','GRY','GRZ','OPL','OPR','ORL','ORR','OYL','OYR'};
+
+colorspec = {[0.9 0.3 0.9];[0.6 0.5 0.2];[0.9 0.8 0.1];[0.8 1 0];[0 1 1];[1 0 0];[0 1 0];[0 0 1]; [1 0.5 0.1];[0 0 0];};
+
+SensorNames = {'ALX','ALY','ALZ','ARX','ARY','ARZ','EMG0L','EMG0R','EMG1L','EMG1R','EMG2L','EMG2R','EMG3L','EMG3R','EMG4L','EMG4R','EMG5L','EMG5R','EMG6L','EMG6R','EMG7L','EMG7R','GLX','GLY','GLZ','GRX','GRY','GRZ','OPL','OPR','ORL','ORR','OYL','OYR'};
+cd .;
 for i = 1:num_components
-    x = 1:55;
+    figure('units','normalized','outerposition',[0 0 1 1]);
+    x = 1:34;
     y = [];
    
    for j = 1:length(pca_output)
        y(:,j) = pca_output{1,j}{1,i};
         
    end
-   colorstring = 'bkrgycmwbk';
    figure(i); cla;
     
    
    for k = 1:length(pca_output)
-        disp(k);
-        hold on
-    % plot(x,y(:, k), 'Color', [rand(1) rand(1) rand(1)])
-        plot(x,y(:, k), 'Color', colorstring(k))
-    % set(gca,'XTick',1:34,'XTickLabel',SensorNames);
-    %xtickangle(90);
+       hold on
+       plot(x,y(:, k),'Color', colorspec{k});set(gca,'XTick',1:34,'XTickLabel',SensorNames);xtickangle(-45);
+     [~, hobj, ~, ~] = legend({'ABOUT','AND','CAN','COP','DEAF', 'DECIDE', 'FATHER', 'FIND', 'GO OUT', 'HEARING'},'Fontsize',12,'Location','Northwest');
+    hl = findobj(hobj,'type','line');
+    set(hl,'LineWidth',2.5);
+    ht = findobj(hobj,'type','text');
+    set(ht,'FontSize',12);
+    ylabel('latent DWT values');
+    xlabel('Sensor names');
+    title("PCA Output");
+    grid on
+
    end
+   filename = "overlapped_pca_dwt_component_"+i
+    saveas(gca, fullfile(char(path+"/images"), char(filename)), 'jpeg');
+    
 end
--- task3.m
+++ task3.m
@@ -4,13 +4,12 @@ pca_input={};
 pca_output={};
 num_components = 3;
 
-for k = 1 : length(allGestures)
-    tempArray = mean(allGestures{k});
-    %tempArray = mean(allGestures{k}(:,:,1:6));
-    tempArr_size = size(tempArray);
-    temp = reshape(tempArray,[tempArr_size(1)*tempArr_size(2),tempArr_size(3)]);
-    pca_input{k}=temp;
-    [coeff,score,latent]  = pca(pca_input{k}.','NumComponents',num_components);
+    for j = 1 : length(allGestures)
+        pca_input{j} = dwtStats(:,:,j);
+    end
+
+ for k = 1 : length(allGestures)
+    [coeff,score,latent]  = pca(pca_input{k},'NumComponents',num_components);
     components= {};
     for l = 1:num_components
         components{l} = coeff(:,l:l);
